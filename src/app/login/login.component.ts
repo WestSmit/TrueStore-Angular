42df69eea7356001cf1d14a72ce431739b5792df
WestSmit <movinguser@gmail.com>
2020-08-19 01:08:24 +0300

    some changes in login and registration methods

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MustMatch } from "../helpers/mustMuch.validator";
 import { LoginService } from "../services/login.service";
+import { NotificationService } from '../services/notification.service';
 
 declare var $: any;
 
@@ -16,25 +17,40 @@ export class LoginComponent implements OnInit {
 
   message: string = "";
   errorMessage: string = "";
+  inProgress = false;
 
   constructor(
     private loginService: LoginService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private notificationService: NotificationService
   ) {}
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group(
       {
-        firstName: ['', [Validators.required, Validators.pattern("[A-Za-z]{2,}")]],
-        secondName: ['', [Validators.required, Validators.pattern("[A-Za-z]{2,}")]],
-        userName: ['', [Validators.required, Validators.pattern("[A-Za-z0-9]{2,}")]],
-        email: ['', [Validators.required, Validators.email]],
-        phoneNumber: ['', [Validators.required, Validators.pattern("[0-9]{11}")]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-        confirmPassword: ['', Validators.required],
-      }, {
-      validator: MustMatch('password', 'confirmPassword')
-    }
+        firstName: [
+          "",
+          [Validators.required, Validators.pattern("[A-Za-z]{2,}")],
+        ],
+        secondName: [
+          "",
+          [Validators.required, Validators.pattern("[A-Za-z]{2,}")],
+        ],
+        userName: [
+          "",
+          [Validators.required, Validators.pattern("[A-Za-z0-9]{2,}")],
+        ],
+        email: ["", [Validators.required, Validators.email]],
+        phoneNumber: [
+          "",
+          [Validators.required, Validators.pattern("[0-9]{11}")],
+        ],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ["", Validators.required],
+      },
+      {
+        validator: MustMatch("password", "confirmPassword"),
+      }
     );
     this.loginForm = this.formBuilder.group({
       userName: ["", Validators.required],
@@ -53,34 +69,58 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.invalid) {
       return;
     }
-
-    var result = await this.loginService.logIn(
-      this.loginForm.controls.userName.value,
-      this.loginForm.controls.password.value
-    );
-    if (result) {
-      this.message = this.loginService.message;
-      this.loginForm.reset();
-    } else {
-      this.errorMessage = this.loginService.message;
-    }
+    this.inProgress = true;
+    this.loginService
+      .logIn(
+        this.loginForm.controls.userName.value,
+        this.loginForm.controls.password.value
+      )
+      .subscribe(
+        (res) => {
+          var user = res.user;
+          localStorage.setItem("CurrentUser", JSON.stringify(user));
+          this.inProgress = false;
+          this.closeSignInForm();
+          this.notificationService.addNotice(
+            "Welcome!!! You can go to",
+            "/MyAccount/Details",
+            "account details"
+          );
+          if (user.emailConfirmed == false) {
+            this.notificationService.addNotice(
+              "Your email is mot confirmed. We have sended you letter with link to confirm. Check your inbox..."
+            );
+          }          
+        },
+        (error) => {
+          this.inProgress = false;
+          this.errorMessage = "Loging failed";
+        });
   }
 
-  async SignUp() {
+  signUp() {
     this.message = "";
     this.errorMessage = "";
+    this.inProgress = true;
     if (this.registerForm.invalid) {
       return;
     }
-    var result = await this.loginService.SingUp(this.registerForm.value);
-    if (result) {
-      this.message = this.loginService.message;
-      this.registerForm.reset();
-    } else {
-      this.errorMessage = this.loginService.message;
-    }
+    this.loginService.singUp(this.registerForm.value).subscribe(
+      () => {
+        this.message = "Welcome!";
+        this.inProgress = false;
+      },
+      (error) => {
+        if (error.error && error.error[0] && error.error[0].description) {
+          this.errorMessage = error.error[0].description;
+        } else {
+          this.errorMessage = "Registration failed";
+        }
+        this.inProgress = false;        
+      }
+    );
   }
-  
+
   closeSignInForm() {
     $("#SignInModal").hide();
     this.message = "";
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -19,76 +19,37 @@ export class LoginService {
   public loggedIn = false;
   private currentUser: User;
   get user(): User {
+    this.currentUser = JSON.parse(localStorage.getItem("CurrentUser"));
+    if (this.currentUser) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
     return this.currentUser;
   }
   public message: string = "";
 
   constructor(
     private http: HttpClient,
-    private cookieService: CookieService,
-    private notificationService: NotificationService
-  ) {
-    this.currentUser = JSON.parse(localStorage.getItem("CurrentUser"));
+    private cookieService: CookieService
+  ) {}
 
-    if (this.currentUser != undefined && this.currentUser != null) {
-      this.loggedIn = true;
-    } else {
-      this.loggedIn = false;
-    }
-  }
-
-  async logIn(userName: string, password: string): Promise<boolean> {
+  logIn(userName: string, password: string) {
     let headers = new HttpHeaders({ "Content-Type": "application/json" });
     let body = { userName: userName, password: password };
     let httpOptions = {
       headers: headers,
       withCredentials: true,
     };
-    var model = await this.http
-      .post<UserResult>(`${environment.apiUrl}User/Login`, body, httpOptions)
-      .toPromise();
-    this.message = model.message;
-    if (model.successed == false) {
-      this.loggedIn = false;
-      return false;
-    } else {
-      this.currentUser = model.user;
-      this.loggedIn = true;
-      localStorage.setItem("CurrentUser", JSON.stringify(this.currentUser));
-      this.notificationService.addNotice(
-        "Welcome!!! You can go to",
-        "/MyAccount/Details",
-        "account details"
-      );
-      if (this.currentUser.emailConfirmed == false) {
-        this.notificationService.addNotice(
-          "Your email is mot confirmed. We have sended you letter with link to confirm. Check your inbox..."
-        );
-      }
-      return true;
-    }
+    return this.http.post<UserResult>(`${environment.apiUrl}User/Login`, body, httpOptions);    
   }
 
-  async SingUp(user: User): Promise<boolean> {
+  singUp(user: User) {
     let headers = new HttpHeaders({ "Content-Type": "application/json" });
     let httpOptions = {
       headers: headers,
     };
-    var result = await this.http
-      .post<BaseModel>(
-        `${environment.apiUrl}User/Registration`,
-        user,
-        httpOptions
-      )
-      .toPromise();
-    console.log(result);
-    this.message = result.message;
-    if (result.successed != true) {
-      this.loggedIn = false;
-      return false;
-    } else {
-      return true;
-    }
+    return this.http.post(`${environment.apiUrl}User/Registration`, user, httpOptions)
   }
 
   refresh() {
